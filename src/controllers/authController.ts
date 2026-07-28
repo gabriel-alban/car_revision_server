af3eb3d7fcdc6c18f74d9f64cbad3840b9131c64
Gabriel Alban <albangabriel42@gmail.com>
2026-07-28 10:13:15 +0300

    finish auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,12 @@ import hash from "../helpers/hash.js";
 
 class AuthController {
     async me(req: Request, res: Response) {
-
+        try {
+            const user = User.findById(req.body._id).select("-password");
+            return res.status(200).json(user);
+        } catch(err) {
+            return res.status(500).json({error: 'Something went wrong!'})
+        }
     }
 
     async register(req: Request, res: Response) {
@@ -26,7 +31,21 @@ class AuthController {
     }
 
     async login(req: Request, res: Response) {
+        try {
+            let user = await User.findOne({email: req.body.email});
 
+            if(!user) return res.status(400).json({error: 'Invalid Email'});
+
+            const validPass = await hash.compare(req.body.password, user.password);
+
+            if(!validPass) return res.status(400).json({error: 'Invalid Password'});
+
+            const token = user.generateToken();
+
+            return res.header('X-AUTH-TOKEN', token).status(200).json(user);
+        } catch(err) {
+            return res.status(500).json({error: 'Something went wrong'});
+        }
     }
 }
 

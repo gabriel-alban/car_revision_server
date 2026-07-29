2ef156a738ce68a4224ce516d5bb9905fc3d9508
Gabriel Alban <albangabriel42@gmail.com>
2026-07-29 10:41:15 +0300

    Some changes

--- src/controllers/revisionController.ts
+++ src/controllers/revisionController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { Revision } from '../models/revisionModel.js';
+import { Car } from '../models/carModel.js';
 
 class RevisionController {
     async getRevisions(req: Request, res: Response) {
@@ -21,8 +22,13 @@ class RevisionController {
         if (!carId) return res.status(400).json({error: "No car selected."});
 
         try {
-            const revision = new Revision(req.body);
+            const revision = new Revision({...req.body, car: carId});
             await revision.save();
+            
+            await Car.updateOne(
+                {_id: carId},
+                {$max: { km_range: revision.current_km_number}}
+            )
 
             return res.status(201).json({revision, message: 'Success!'});
         } catch(err) {
@@ -37,7 +43,7 @@ class RevisionController {
         if (!id) return res.status(400).json({error: 'No revision selected.'});
 
         try {
-            await Revision.findByIdAndDelete(id);
+            await Revision.findOneAndDelete({_id: id, car: carId});
             return res.status(200).json({message: 'Success'});
         } catch(err) {
             return res.status(500).json({error: 'Something went wrong!'});
